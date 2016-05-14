d47f4772dce299dbcfa55ea5615990e967a24222
thesteveaitken <thesteveaitken@gmail.com>
2016-05-14 12:11:26 -0400

    updated figaro info

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -2,5 +2,5 @@ class Listing < ActiveRecord::Base
 	has_attached_file 	:image, styles: { medium: "200x>", thumb: "100x100>" }, default_url: "no_image.gif",
 						:storage => :dropbox,
     					:dropbox_credentials => Rails.root.join("config/dropbox.yml")
- 	validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+ 	validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
